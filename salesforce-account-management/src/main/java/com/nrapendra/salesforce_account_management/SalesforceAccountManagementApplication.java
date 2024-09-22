5c8bd6229cc9d3efe0181452939c591ff0491297
Nrapendra Trivedi <trivajay259@gmail.com>
2024-09-23 01:38:40 +0200

    created account-management-application skeleton project

---
+++ salesforce-account-management/src/main/java/com/nrapendra/salesforce_account_management/SalesforceAccountManagementApplication.java
@@ -0,0 +1,13 @@
+package com.nrapendra.salesforce_account_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesforceAccountManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesforceAccountManagementApplication.class, args);
+	}
+
+}
---
+++ salesforce-account-management/src/test/java/com/nrapendra/salesforce_account_management/SalesforceAccountManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nrapendra.salesforce_account_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesforceAccountManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
