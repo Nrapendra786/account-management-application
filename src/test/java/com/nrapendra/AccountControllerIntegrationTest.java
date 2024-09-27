639e2750517e3fbc8cac8ef4ac610fcf242f4476
Nrapendra Trivedi <trivajay259@gmail.com>
2024-09-27 18:08:41 +0200

    updated TestUtil package in AccountControllerIntegrationTest.java

--- src/test/java/com/nrapendra/AccountControllerIntegrationTest.java
+++ src/test/java/com/nrapendra/AccountControllerIntegrationTest.java
@@ -17,7 +17,7 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import com.nrapendra.TestUtil;
+import com.nrapendra.TestUtil.*;
 import java.net.URI;
 import java.util.Map;
 import java.util.Random;
